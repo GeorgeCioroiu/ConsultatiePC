70c37efac879155c4fcec24c2ace9f49f17e8e66
Student <carmen_nicoleta_97@yahoo.com>
2016-12-16 16:32:02 +0200

    am trimis angajat

--- proiect/angajat.c
+++ proiect/angajat.c
@@ -1,4 +1,6 @@
 #include "angajat.h"
+#include <stdio.h>
+
 void readAngajat(FILE *inputFile, om *angajat){
     fscanf(inputFile,"%d",((*angajat).idAngajat));
     fscanf(inputFile,"%d",((*angajat).idLibrarie));
@@ -9,4 +11,16 @@ void printAngajat(FILE *outputFile, om angajat) {
     fprintf(outputFile,"%d",angajat.idAngajat);
     fprintf(outputFile,"%d",angajat.idLibrarie);
     fprintf(outputFile,"%d",angajat.salariu);
+}
+
+int idBestSallary(om v[1000], int nr){
+    int max=0,i,id;
+    for(i=0;i<nr;i++)
+    {
+        if(v[i].salariu>max) {
+            max=v[i].salariu;
+            id=v[i].idAngajat;
+        }
+    }
+    return id;
 }
