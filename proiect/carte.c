c0e54f81b8005548e8de974a926e5a18eda4c736
Gabriel Urian <Gabriel Urian>
2016-12-16 16:18:02 +0200

    carte.c v4

--- proiect/carte.c
+++ proiect/carte.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-Carte* ReadCarte(FILE *nameFile, Carte *adressBook){
+void ReadCarte(FILE *nameFile, Carte *adressBook){
     fscanf(nameFile, "%d", &adressBook->id);
     fscanf(nameFile, "%d", &adressBook->idAutor);   fscanf(nameFile, "%s");
     fscanf(nameFile, "%s", &adressBook->nume);
     fscanf(nameFile, "%d", &adressBook->anLansare);
     fscanf(nameFile, "%d", &adressBook->pret);      fscanf(nameFile, "%s");
     fscanf(nameFile, "%s", &adressBook->gen);
-    return adressBook;
 }
 
-void PrintCarte(FILE *nameFile, Carte *book) {
+void PrintCarte(FILE *nameFile, Carte book) {
     fprintf(nameFile, "id=%d \n", book->id);
     fprintf(nameFile, "id Autor=%d \n", book->idAutor);
     fprintf(nameFile, "Nume=%s \n", book->nume);
